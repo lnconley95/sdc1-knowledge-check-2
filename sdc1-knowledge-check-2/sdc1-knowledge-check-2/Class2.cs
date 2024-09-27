712eb3006efbd2d9252800d6714bc92dafd10093
Lindsey <164207764+lnconley95@users.noreply.github.com>
2024-09-26 20:07:01 -0400

    I did it!
    I think

---
+++ sdc1-knowledge-check-2/sdc1-knowledge-check-2/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdc1_knowledge_check_2
+{
+    public class FictionBook : Books
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+    }
+}
---
+++ sdc1-knowledge-check-2/sdc1-knowledge-check-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using sdc1_knowledge_check_2;
+using System;
+using System.Collections.Generic;
+
+namespace YourNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("How many Fiction books do you want to add? ");
+                var numberOfBooks = int.Parse(Console.ReadLine());
+
+                var bookList = new List<FictionBook>();
+                for (int i = 0; i < numberOfBooks; i++)
+                {
+                    var fictionBook = new FictionBook();
+
+                    Console.WriteLine("Enter the title of the Book: ");
+                    fictionBook.Title = Console.ReadLine();
+
+                    Console.WriteLine("Enter the Author of the Book: ");
+                    fictionBook.Author = Console.ReadLine();
+
+                    Console.WriteLine("Enter the number of Pages in the Book: ");
+                    fictionBook.NumberOfPages = int.Parse(Console.ReadLine());
+
+                    bookList.Add(fictionBook);
+                }
+
+                foreach (var book in bookList)
+                {
+                    Console.WriteLine("Book Title: " + book.Title);
+                    Console.WriteLine("Author: " + book.Author);
+                    Console.WriteLine("Number of Pages: " + book.NumberOfPages);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
